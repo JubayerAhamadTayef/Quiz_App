ebd5794c0437fc9397e7b6cc7a912fd7db7af517
JubayerAhamadTayef <jubayerahmedtayef.579462@gmail.com>
2024-09-03 17:43:29 +0600

    Start using binding!

--- app/src/main/java/com/example/quiz_app/MainActivity.kt
+++ app/src/main/java/com/example/quiz_app/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.quiz_app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.quiz_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
