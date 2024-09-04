a640f256252d3951da1792bd6ce2889ac4844e05
JubayerAhamadTayef <jubayerahmedtayef.579462@gmail.com>
2024-09-04 08:06:30 +0600

    Start using binding!

--- app/src/main/java/com/example/quiz_app/QuizActivity.kt
+++ app/src/main/java/com/example/quiz_app/QuizActivity.kt
@@ -5,12 +5,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.quiz_app.databinding.ActivityQuizBinding
 
 class QuizActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityQuizBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
+        binding = ActivityQuizBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz)
+        setContentView(binding.root)
 
     }
 }
