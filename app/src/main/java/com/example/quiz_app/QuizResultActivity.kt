4f289450355ba6975bf4bc61813abe8ab8cfa482
JubayerAhamadTayef <jubayerahmedtayef.579462@gmail.com>
2024-09-04 10:55:30 +0600

    Complete getting value from Intent!

--- app/src/main/java/com/example/quiz_app/QuizResultActivity.kt
+++ app/src/main/java/com/example/quiz_app/QuizResultActivity.kt
@@ -1,5 +1,6 @@
 package com.example.quiz_app
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.quiz_app.databinding.ActivityQuizResultBinding
@@ -8,12 +9,21 @@ class QuizResultActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityQuizResultBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
 
         binding = ActivityQuizResultBinding.inflate(layoutInflater)
 
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.apply {
+
+            totalSkip.text = "Total Skip Answer: ${intent.getIntExtra("Total Skip Answer:", 0)}"
+            totalCorrect.text = "Total Correct Answer: ${intent.getIntExtra("Total Correct Answer:", 0)}"
+            totalWrong.text = "Total Wrong Answer: ${intent.getIntExtra("Total Wrong Answer:", 0)}"
+
+        }
 
     }
 }
