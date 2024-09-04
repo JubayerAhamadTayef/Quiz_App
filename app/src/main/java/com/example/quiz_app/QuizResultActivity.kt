5bbfcecc99458850df4a29d0532df263a3d4a202
JubayerAhamadTayef <jubayerahmedtayef.579462@gmail.com>
2024-09-04 10:42:27 +0600

    Remove unnecessary code and start using binding!

--- app/src/main/java/com/example/quiz_app/QuizResultActivity.kt
+++ app/src/main/java/com/example/quiz_app/QuizResultActivity.kt
@@ -1,20 +1,19 @@
 package com.example.quiz_app
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.quiz_app.databinding.ActivityQuizResultBinding
 
 class QuizResultActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityQuizResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        binding = ActivityQuizResultBinding.inflate(layoutInflater)
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_quiz_result)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        setContentView(binding.root)
+
     }
 }
